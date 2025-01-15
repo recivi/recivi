e1de64ff88eb090a46879830a54feaf25a0d0bf7
Dhruv Bhanushali <dhruv_b@live.com>
2025-01-16 00:02:10 +0530

    Add validated examples

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,5 +1,9 @@
 // Note that globs should not overlap as they can lead to race conditions.
 export default {
+  'packages/schema/examples/*.json': [
+    'prettier --write',
+    'pnpm -F schema validate',
+  ],
   '*.{js,ts,astro}': ['eslint --fix', 'prettier --write'],
-  '*.{css,html,json,md,mdx,pcss,svg,yml}': ['prettier --write'],
+  '*.{css,html,md,mdx,pcss,svg,yml}': ['prettier --write'],
 }
