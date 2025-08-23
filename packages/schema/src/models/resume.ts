4a1ebb1f6679868d160d4afbe7d6e45ee2938ee2
Dhruv Bhanushali <dhruv_b@live.com>
2025-08-23 19:02:28 +0400

    Fix bug in registration of root schema

--- packages/schema/src/models/resume.ts
+++ packages/schema/src/models/resume.ts
@@ -7,40 +7,45 @@ import { type Org, orgSchema } from '@/models/work/org'
 import { type Epic, epicSchema } from '@/models/creations/epic'
 import { type Institute, instituteSchema } from '@/models/education/institute'
 
-export const resumeSchema = z.object({
-  $schema: z
-    .url()
-    .optional()
-    .default('https://recivi.pages.dev/schemas/recivi-resume.json')
-    .register(primaryRegistry, {
-      description:
-        'the URL to the JSON schema that should be used to validate this document',
+export const resumeSchema = z
+  .object({
+    $schema: z
+      .url()
+      .optional()
+      .default('https://recivi.pages.dev/schemas/recivi-resume.json')
+      .register(primaryRegistry, {
+        description:
+          'the URL to the JSON schema that should be used to validate this document',
+      }),
+    bio: bioSchema.clone().register(primaryRegistry, {
+      description: 'the identity and personal information of a person',
     }),
-  bio: bioSchema.clone().register(primaryRegistry, {
-    description: 'the identity and personal information of a person',
-  }),
-  creations: z
-    .array(epicSchema)
-    .optional()
-    .default([])
-    .register(primaryRegistry, {
+    creations: z
+      .array(epicSchema)
+      .optional()
+      .default([])
+      .register(primaryRegistry, {
+        description:
+          'the creations of a person, which consists of projects under various epics',
+      }),
+    education: z
+      .array(instituteSchema)
+      .optional()
+      .default([])
+      .register(primaryRegistry, {
+        description:
+          'the education of a person, which consists of certifications from various insitutes',
+      }),
+    work: z.array(orgSchema).optional().default([]).register(primaryRegistry, {
       description:
-        'the creations of a person, which consists of projects under various epics',
+        'the work experience of a person, which consists of positions of responsibility in various organisations',
     }),
-  education: z
-    .array(instituteSchema)
-    .optional()
-    .default([])
-    .register(primaryRegistry, {
-      description:
-        'the education of a person, which consists of certifications from various insitutes',
-    }),
-  work: z.array(orgSchema).optional().default([]).register(primaryRegistry, {
+  })
+  .register(primaryRegistry, {
     id: 'Resume',
     description:
-      'the work experience of a person, which consists of positions of responsibility in various organisations',
-  }),
-})
+      'comprehensive information, both personal and professional, about a person; This is the root of the Récivi schema.',
+  })
 
 export type Resume = Omit<
   z.infer<typeof resumeSchema>,
