8004f94b7ecec6edb7c2c4382b57b517301d281b
Dhruv Bhanushali <dhruv_b@live.com>
2026-01-10 13:28:01 +0400

    Move schema examples into top-level examples directory

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,6 +1,6 @@
 // Note that globs should not overlap as they can lead to race conditions.
 export default {
-  'packages/schema/examples/*.json': [
+  'examples/schema/*.json': [
     // Runs formatter, linter and import sorting to the requested files.
     'biome check --write --no-errors-on-unmatched',
     'pnpm -F schema validate',
