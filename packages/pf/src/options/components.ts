31a1c44767818a81a5608ba0ceff4daebf7fa39c
Dhruv Bhanushali <dhruv_b@live.com>
2026-07-30 10:49:38 +0400

    Use the component names and type from the new file created by the `barrel` script

--- packages/pf/src/options/components.ts
+++ packages/pf/src/options/components.ts
@@ -1,47 +1,8 @@
 import { z } from "astro/zod";
 
+import { componentNames, type ComponentName } from "../components/index";
 import { primaryRegistry } from "../registries/primary";
-import { getAllComponents } from "../utils/layouts";
 
-// If a component is changed, you should also update `virtual.d.ts`.
-const componentNames = [
-	"Anchor",
-	"Brand",
-	"Breadcrumbs",
-	"Comms",
-	"Elements",
-	"Entity",
-	"EpicCard",
-	"EpicDetails",
-	"Footer",
-	"Header",
-	"Icon",
-	"InstituteCard",
-	"Letterhead",
-	"Nav",
-	"NowUpdate",
-	"OrgCard",
-	"OrgDetails",
-	"Period",
-	"PfDate",
-	"Post",
-	"Skill",
-	"Table",
-	"TechStack",
-	"Toc",
-] as const;
-type ComponentName = (typeof componentNames)[number];
-
-// Keep the above list synced with the actual components.
-const allComponents = await getAllComponents();
-allComponents.forEach((component, idx) => {
-	if (componentNames[idx] !== component) {
-		throw new Error(`Component names mismatch occurred.`);
-	}
-});
-
-// `z.object` ensures that every layout named above has an entry. We are not
-// using `z.record` because we want to allow missing keys in the input.
 export const componentsSchema = z
 	.object(
 		Object.fromEntries(
