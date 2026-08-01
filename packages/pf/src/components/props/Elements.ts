6de748911cdcb152d2e6686b4baf1c61c5007e8f
Dhruv Bhanushali <dhruv_b@live.com>
2026-08-01 05:44:41 +0400

    Break declaration into smaller interfaces to avoid a layout that needs space-based alignment

--- packages/pf/src/components/props/Elements.ts
+++ packages/pf/src/components/props/Elements.ts
@@ -1,16 +1,16 @@
 import type { LinkTag, MetaTag } from "../../types/tags";
 import type { BaseProps } from "./base";
 
-export type ElementsProps = BaseProps &
-	(
-		| {
-				tag: "link";
-				/** the attributes of the `<link>` tags to render */
-				items: LinkTag[];
-		  }
-		| {
-				tag: "meta";
-				/** the attributes of the `<meta>` tags to render */
-				items: MetaTag[];
-		  }
-	);
+interface LinkElementsProps {
+	tag: "link";
+	/** the attributes of the `<link>` tags to render */
+	items: LinkTag[];
+}
+
+interface MetaElementsProps {
+	tag: "meta";
+	/** the attributes of the `<meta>` tags to render */
+	items: MetaTag[];
+}
+
+export type ElementsProps = BaseProps & (LinkElementsProps | MetaElementsProps);
