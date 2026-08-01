21a7e26dff8e867411efafa3e51accb3c422f40b
Dhruv Bhanushali <dhruv_b@live.com>
2026-08-01 21:19:04 +0400

    Completely revamp the Astro integration

--- packages/pf/src/index.ts
+++ packages/pf/src/index.ts
@@ -7,21 +7,282 @@
 /// <reference path="./virtual.d.ts"/>
 /// <reference path="./components.d.ts"/>
 
+import { fileURLToPath } from "node:url";
 import { styleText } from "node:util";
 
 import mdx from "@astrojs/mdx";
 import type { AstroIntegration } from "astro";
-import { addVirtualImports } from "astro-integration-kit";
 
-import { captureOgImages } from "./jobs/capture_og";
-import { watchDatafile } from "./jobs/watch_datafile";
+import { componentNames } from "./components/index";
+import { generateOg } from "./jobs/generate_og";
+import { generatePdf } from "./jobs/generate_pdf";
 import { type Options, optionsSchema, type ParsedOptions } from "./options";
 import { layoutNames } from "./options/css";
 import { loadReciviData } from "./recivi/load";
-import { defaultLayout } from "./remark/default_layout";
+import { satteriProcessor } from "./satteri/default_layout";
 import type { ProjectContext } from "./types/project_context";
+import { debounce } from "./utils/debounce";
 import { getAbsolutePath, isLocalFile } from "./utils/paths";
 import { publicFileExists } from "./utils/project_context";
+import { getVirtualImport, type MutableVirtualImport } from "./vite/virtual_import";
+
+type Hooks = AstroIntegration["hooks"];
+type ConfigSetupParams = Parameters<NonNullable<Hooks["astro:config:setup"]>>[0];
+type ServerSetupParams = Parameters<NonNullable<Hooks["astro:server:setup"]>>[0];
+type RoutesResolvedParams = Parameters<NonNullable<Hooks["astro:routes:resolved"]>>[0];
+
+/**
+ * Resolve some options that use relative paths to their absolute paths.
+ *
+ * These paths are resolved relative to the Astro config file. The changes are
+ * made in-place to the passed `options` object.
+ *
+ * @param options the parsed options in which to resolve relative paths
+ * @param configRoot the root directory of the Astro config file
+ */
+function resolveOptions(options: ParsedOptions, configRoot: URL) {
+	options.reciviDataFile = options.reciviDataFile.startsWith(".")
+		? getAbsolutePath(options.reciviDataFile, configRoot)
+		: options.reciviDataFile;
+
+	for (const layoutName of layoutNames) {
+		options.css.customCss[layoutName] = options.css.customCss[layoutName].map((file) =>
+			file.startsWith(".") ? getAbsolutePath(file, configRoot) : file,
+		);
+	}
+
+	for (const componentName of componentNames) {
+		const name = options.components[componentName];
+		options.components[componentName] = name.startsWith(".")
+			? getAbsolutePath(name, configRoot)
+			: name;
+	}
+}
+
+/**
+ * Collect information about the Astro project and return a project context object.
+ *
+ * @param params the parameters provided by the Astro integration hook
+ * @param options the parsed options for the integration
+ * @returns a project context object containing information about the Astro project
+ */
+async function createProjectContext(
+	params: ConfigSetupParams,
+	options: ParsedOptions,
+): Promise<ProjectContext> {
+	const { site, base, trailingSlash } = params.config;
+	const publicDir = fileURLToPath(params.config.publicDir);
+	const prefix = options.favicon.fileNames.manifestIconsPrefix;
+	const iconChecks = [192, 512].map((size) =>
+		publicFileExists({ publicDir }, `${prefix}${size}.png`),
+	);
+	const hasManifestIcons = (await Promise.all(iconChecks)).some(Boolean);
+	return { site, base, trailingSlash, publicDir, hasManifestIcons };
+}
+
+/**
+ * Set up virtual imports for the project.
+ *
+ * @param params the parameters provided by the Astro integration hook
+ * @param options the parsed options for the integration
+ * @param projectContext the project context
+ * @returns a record of virtual imports
+ */
+function setVirtualImports(
+	params: ConfigSetupParams,
+	options: ParsedOptions,
+	projectContext: ProjectContext,
+): Record<string, MutableVirtualImport> {
+	const imports = {
+		"pf/config": `export default ${JSON.stringify(options)}`,
+		"pf/project-context": `export default ${JSON.stringify(projectContext)}`,
+
+		// Navigation info is determined during route resolution.
+		"pf/nav": `export default {}`,
+
+		// Récivi data is loaded later and updated.
+		"recivi/data": `export default {}`,
+
+		"pf/components": Object.entries(options.components)
+			.map(([name, path]) => `export { default as ${name} } from '${path}';`)
+			.join("\n"),
+		...Object.fromEntries(
+			layoutNames.map((layoutName) => [
+				`pf/custom-css/${layoutName}`,
+				options.css.customCss[layoutName].map((file) => `import '${file}';`).join("\n"),
+			]),
+		),
+	};
+	const virtualImports = Object.fromEntries(
+		Object.entries(imports).map(([name, code]) => [name, getVirtualImport(name, code)]),
+	);
+
+	params.updateConfig({
+		vite: {
+			plugins: Object.values(virtualImports).map(({ plugin }) => plugin),
+		},
+	});
+
+	return virtualImports;
+}
+
+/**
+ * Inject custom endpoints into the Astro project.
+ *
+ * @param params the parameters provided by the Astro integration hook
+ * @param options the parsed options for the integration
+ * @param projectContext the project context
+ */
+function injectRoutes(
+	params: ConfigSetupParams,
+	options: ParsedOptions,
+	projectContext: ProjectContext,
+) {
+	if (projectContext.site) {
+		params.injectRoute({
+			pattern: "og/[...path].png",
+			entrypoint: "@recivi/pf/routes/defs/OgRender.astro",
+		});
+		params.injectRoute({ pattern: "rss.xml", entrypoint: "@recivi/pf/routes/rss.ts" });
+	}
+	if (options.favicon.isEnabled && projectContext.hasManifestIcons) {
+		params.injectRoute({
+			pattern: "manifest.webmanifest",
+			entrypoint: "@recivi/pf/routes/manifest.ts",
+		});
+	}
+}
+
+/**
+ * Configure Markdown processing for the project.
+ *
+ * @param params the parameters provided by the Astro integration hook
+ */
+function configureMarkdown(params: ConfigSetupParams) {
+	params.updateConfig({
+		markdown: {
+			shikiConfig: {
+				themes: {
+					light: "catppuccin-latte",
+					dark: "catppuccin-mocha",
+				},
+			},
+			processor: satteriProcessor,
+		},
+		integrations: [mdx({ optimize: true })],
+	});
+}
+
+/**
+ * Configure the Astro project with the given options.
+ *
+ * This is part of the `astro:config:setup` hook.
+ *
+ * @param params the parameters provided by the Astro integration hook
+ * @param options the parsed options for the integration
+ * @returns an object containing the project context and virtual imports
+ */
+async function configureAstro(params: ConfigSetupParams, options: ParsedOptions) {
+	resolveOptions(options, params.config.root);
+	// The `options` have now been mutated to contain absolute paths.
+
+	const projectContext = await createProjectContext(params, options);
+	const virtualImports = setVirtualImports(params, options, projectContext);
+	injectRoutes(params, options, projectContext);
+	configureMarkdown(params);
+
+	return { projectContext, virtualImports };
+}
+
+/**
+ * Reload the Récivi data file, update the associated virtual module and trigger
+ * a full page reload in the Astro dev server.
+ *
+ * This is used by hooks `astro:config:setup` and `astro:server:setup`.
+ *
+ * @param params the parameters provided by the Astro integration hook
+ * @param options the parsed options for the integration
+ * @param reciviDataPlugin the virtual import plugin for the Récivi data module
+ */
+async function reloadReciviData(
+	params: ServerSetupParams | ConfigSetupParams,
+	options: ParsedOptions,
+	reciviDataPlugin: MutableVirtualImport,
+) {
+	const reciviData = await loadReciviData(options.reciviDataFile);
+	reciviDataPlugin.update(`export default ${JSON.stringify(reciviData)}`);
+
+	if ("server" in params) {
+		const module = params.server.moduleGraph.getModuleById(reciviDataPlugin.id);
+		if (module) {
+			params.server.moduleGraph.invalidateModule(module);
+		}
+		params.server.hot.send({ type: "full-reload" });
+	}
+}
+
+/**
+ * Identify the dynamic routes for blog posts and resume subpages, and update
+ * the associated virtual module.
+ *
+ * @param params the parameters provided by the Astro integration hook
+ * @param virtualPfNav the virtual import plugin for the PF navigation module
+ */
+function reloadPfNav(params: RoutesResolvedParams, virtualPfNav: MutableVirtualImport) {
+	const findRoute = (slugName: string) =>
+		params.routes.find((route) => route.params.includes(slugName))?.pattern;
+
+	const navInfo = {
+		blogPost: findRoute("postSlug"),
+		resumeEpic: findRoute("epicSlug"),
+		resumeOrg: findRoute("orgSlug"),
+		resumeInstitute: findRoute("instituteSlug"),
+	};
+	virtualPfNav.update(`export default ${JSON.stringify(navInfo)}`);
+}
+
+function createHooks(options: ParsedOptions): AstroIntegration["hooks"] {
+	let projectContext: ProjectContext;
+	let virtualImports: Record<string, MutableVirtualImport>;
+	let isWatchingDatafile = false;
+
+	return {
+		"astro:config:setup": async (params) => {
+			params.logger.debug(`${styleText("blue", "astro:config:setup")} hook called`);
+
+			({ projectContext, virtualImports } = await configureAstro(params, options));
+
+			const vReciviData = virtualImports["recivi/data"];
+			if (vReciviData) reloadReciviData(params, options, vReciviData);
+		},
+		"astro:routes:resolved": (params) => {
+			const vPfNav = virtualImports["pf/nav"];
+			if (vPfNav) reloadPfNav(params, vPfNav);
+		},
+		"astro:server:setup": (params) => {
+			params.logger.debug(`${styleText("blue", "astro:server:setup")} hook called`);
+
+			const vReciviData = virtualImports["recivi/data"];
+			if (vReciviData && isLocalFile(options.reciviDataFile) && !isWatchingDatafile) {
+				/** the 1s debounced version of `reloadReciviData` */
+				const reloadData = debounce(() => reloadReciviData(params, options, vReciviData), 1000);
+				params.server.watcher.add(options.reciviDataFile);
+				params.server.watcher.on("change", (path) => {
+					if (path === options.reciviDataFile) reloadData();
+				});
+				isWatchingDatafile = true;
+			}
+		},
+		"astro:build:done": async (params) => {
+			params.logger.debug(`${styleText("blue", "astro:build:done")} hook called`);
+
+			const dirPath = fileURLToPath(params.dir);
+			if (projectContext.site)
+				await generateOg(projectContext, dirPath, params.pages, params.logger);
+			await generatePdf(projectContext, dirPath, params.pages, params.logger);
+		},
+	};
+}
 
 /**
  * Set up the Récivi PF Astro integration with the given configuration.
@@ -31,136 +292,8 @@ import { publicFileExists } from "./utils/project_context";
  */
 export default function (options: Options): AstroIntegration {
 	const parsedOptions: ParsedOptions = optionsSchema.parse(options);
-
-	// Declare variables that store state across hooks.
-	let configRoot: URL;
-	let projectContext: ProjectContext;
-
-	// In case of a restart, the `astro:server:setup` hook is called twice.
-	let isWatchingDatafile = false;
-
 	return {
 		name: "@recivi/pf",
-		hooks: {
-			"astro:config:setup": async (params) => {
-				params.logger.debug(`${styleText("blue", "astro:config:setup")} hook called`);
-
-				// Preserve config root for use in other hooks.
-				configRoot = params.config.root;
-
-				// Preserve project context for use in other hooks.
-				const { site, base, trailingSlash, root, srcDir, publicDir, build } = params.config;
-				const hasManifestIcons = await Promise.all(
-					[192, 512].map((size) => {
-						const prefix = parsedOptions.favicon.fileNames.manifestIconsPrefix;
-						return publicFileExists({ publicDir }, `${prefix}${size}.png`);
-					}),
-				).then((results) => results.some(Boolean));
-				projectContext = {
-					site,
-					base,
-					trailingSlash,
-					root,
-					srcDir,
-					publicDir,
-					build,
-					hasManifestIcons,
-				};
-
-				// Load résumé data file.
-				let { reciviDataFile } = parsedOptions;
-				if (isLocalFile(reciviDataFile)) {
-					reciviDataFile = getAbsolutePath(reciviDataFile, configRoot);
-					params.logger.info(`Using local résumé data file: ${reciviDataFile}`);
-				} else {
-					params.logger.info(`Using remote résumé data file: ${reciviDataFile}`);
-				}
-				const reciviData = await loadReciviData(reciviDataFile);
-
-				// Inject routes.
-				if (projectContext.site) {
-					// Add a route to handle all Open Graph image requests.
-					params.injectRoute({
-						pattern: "og_render/[...path]",
-						entrypoint: "@recivi/pf/routes/defs/OgRender.astro",
-					});
-					params.injectRoute({
-						pattern: "rss.xml",
-						entrypoint: "@recivi/pf/routes/rss.ts",
-					});
-				}
-				if (parsedOptions.favicon.isEnabled && projectContext.hasManifestIcons) {
-					// Inject the manifest route if 192px or 512px icons are present.
-					params.injectRoute({
-						pattern: "manifest.webmanifest",
-						entrypoint: "@recivi/pf/routes/manifest.ts",
-					});
-				}
-
-				// Make useful information globally available via virtual modules.
-				addVirtualImports(params, {
-					name: "@recivi/pf",
-					imports: {
-						"virtual:pf/config": `export default ${JSON.stringify(parsedOptions)}`,
-						"virtual:pf/project-context": `export default ${JSON.stringify(projectContext)}`,
-						"virtual:recivi/data": `export default ${JSON.stringify(reciviData)}`,
-
-						// Each layout gets its own virtual module for custom CSS.
-						...Object.fromEntries(
-							layoutNames.map((layoutName) => [
-								`virtual:pf/custom-css/${layoutName}`,
-								parsedOptions.css.customCss[layoutName]
-									.map((file) => `import '${file}';`)
-									.join("\n"),
-							]),
-						),
-
-						// Imports from this virtual module will automatically resolve to
-						// user-substituted components or fall back to PF components.
-						"virtual:pf/components": Object.entries(parsedOptions.components)
-							.map(([name, path]) => `export { default as ${name} } from '${path}';`,)
-							.join("\n"),
-					},
-				});
-
-				// Make changes to the Astro configuration itself.
-				params.updateConfig({
-					markdown: {
-						shikiConfig: {
-							themes: {
-								// Match the default light props.
-								light: "catppuccin-latte",
-								// Match the default dark props.
-								dark: "catppuccin-mocha",
-							},
-						},
-						remarkPlugins: [defaultLayout],
-					},
-					integrations: [
-						// Ensure PF's MDX integration is included.
-						mdx({ optimize: true }),
-					],
-				});
-			},
-			"astro:server:setup": (params) => {
-				params.logger.debug(`${styleText("blue", "astro:server:setup")} hook called`);
-
-				let { reciviDataFile } = parsedOptions;
-				if (isLocalFile(reciviDataFile) && !isWatchingDatafile) {
-					// Watch local résumé data file for changes.
-					reciviDataFile = getAbsolutePath(reciviDataFile, configRoot);
-					watchDatafile(reciviDataFile, params.server, params.logger);
-					isWatchingDatafile = true;
-				}
-			},
-			"astro:build:done": async (params) => {
-				params.logger.debug(`${styleText("blue", "astro:build:done")} hook called`);
-
-				// Generate Open Graph images.
-				if (projectContext.site) {
-					await captureOgImages(projectContext, params.dir, params.pages, params.logger,);
-				}
-			},
-		},
+		hooks: createHooks(parsedOptions),
 	};
 }
