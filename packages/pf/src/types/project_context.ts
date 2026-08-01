5c00741e5ee6986f3a71433bc698d9174c947b66
Dhruv Bhanushali <dhruv_b@live.com>
2026-08-01 14:55:48 +0400

    Massively reduce the scope of the project context

--- packages/pf/src/types/project_context.ts
+++ packages/pf/src/types/project_context.ts
@@ -1,8 +1,13 @@
 import type { AstroConfig } from "astro";
 
-export interface ProjectContext extends Pick<
-	AstroConfig,
-	"site" | "base" | "trailingSlash" | "root" | "srcDir" | "publicDir" | "build"
-> {
+// Make sure that the `ProjectContext` interface only includes types that can
+// survive JSON serialization, since it will be serialized into a virtual
+// module. For example, interface `AstroConfig` has a `publicDir` field but we
+// don't extend that because it is a `URL` going in, but a `string` coming out.
+
+export interface ProjectContext extends Pick<AstroConfig, "site" | "base" | "trailingSlash"> {
+	/** the path to the project's public directory */
+	publicDir: string;
+	/** whether the site has any of the icons that require `manifest.webmanifest` */
 	hasManifestIcons: boolean;
 }
